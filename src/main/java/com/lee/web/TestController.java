d1160ee4fb787d18955505d19adf75844e015347
libosheng <libosheng@news.cn>
2019-05-17 10:59:19 +0800

    修改为war包模式

--- src/main/java/com/lee/web/TestController.java
+++ src/main/java/com/lee/web/TestController.java
@@ -1,5 +1,7 @@
 package com.lee.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @GetMapping("/test")
     public String test() {
+        logger.info("test request");
         return "test success";
     }
 }
